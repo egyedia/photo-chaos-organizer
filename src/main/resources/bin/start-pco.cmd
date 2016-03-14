85d7a326a865d688ac0b31626c5cfad0d887f2f0
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2016-03-14 01:27:56 -0700

    Launch script for Windows

---
+++ src/main/resources/bin/start-pco.cmd
@@ -0,0 +1,8 @@
+cls
+@echo ---------------------------------------------
+@echo Launching Photo Chaos Organizer from Dubylon
+@echo ---------------------------------------------
+@echo off
+set DIRNAME=%~dp0
+
+java -jar %DIRNAME%/dubylon-photo-chaos-organizer-0.1.0.jar
