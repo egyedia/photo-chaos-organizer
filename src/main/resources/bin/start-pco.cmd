8219f4107e1f846639efa823e2e248b19988c72b
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2016-05-05 00:04:52 -0700

    Version change + config for VLC on Ubuntu

--- src/main/resources/bin/start-pco.cmd
+++ src/main/resources/bin/start-pco.cmd
@@ -1,8 +1,8 @@
 cls
 @echo ----------------------------------------------
-@echo Launching Photo Chaos Organizer 0.1.5-SNAPSHOT
+@echo Launching Photo Chaos Organizer 0.1.5
 @echo ----------------------------------------------
 @echo off
 set DIRNAME=%~dp0
 
-java -jar "%DIRNAME%/photo-chaos-organizer-0.1.5-SNAPSHOT.jar"
+java -jar "%DIRNAME%/photo-chaos-organizer-0.1.5.jar"
