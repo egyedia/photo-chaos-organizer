b60db232225ecbeaa9c3e18e23b323b9404750e3
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2016-07-04 18:44:37 -0700

    Version change

--- src/main/resources/bin/start-pco.cmd
+++ src/main/resources/bin/start-pco.cmd
@@ -1,8 +1,8 @@
 cls
 @echo ----------------------------------------------
-@echo Launching Photo Chaos Organizer 0.1.6-SNAPSHOT
+@echo Launching Photo Chaos Organizer 0.1.7-SNAPSHOT
 @echo ----------------------------------------------
 @echo off
 set DIRNAME=%~dp0
 
-java -jar "%DIRNAME%/photo-chaos-organizer-0.1.6-SNAPSHOT.jar"
+java -jar "%DIRNAME%/photo-chaos-organizer-0.1.7-SNAPSHOT.jar"
